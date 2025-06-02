c918ea2b7a375879d52670d70270066267c0e3c2
nikhil <nikhilsahani242003@gmail.com>
2025-06-02 17:24:33 +0530

    DB SETUP DONE AFTER FIXING ISSUE

---
+++ course-generator/drizzle.config.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import path from "path";
+
+// Load .env from the project root
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
+
+console.log("ENV URL:", process.env.NEXT_PUBLIC_DB_CONNECTION_STRING);
+
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./configs/schema.jsx",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.NEXT_PUBLIC_DB_CONNECTION_STRING,
+    },
+};
+/*
+To use the environment variable for the database URL, update dbCredentials like this:
+
+dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DB_CONNECTION_STRING,
+},
+*/
