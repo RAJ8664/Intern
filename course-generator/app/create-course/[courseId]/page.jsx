ce1f71d6cdfc383c1c686a0fdbfed926d4444435
RAJ ROY <rajr86642@gmail.com>
2025-06-06 01:17:54 +0530

    add generate chapter content feature

--- course-generator/app/create-course/[courseId]/page.jsx
+++ course-generator/app/create-course/[courseId]/page.jsx
@@ -8,12 +8,16 @@ import CourseBasicInfo from './_components/CourseBasicInfo'
 import CourseDetail from './_components/CourseDetail'
 import ChapterList from './_components/ChapterList'
 import { useParams } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import { GenerateCourseLayout_AI } from '../../../configs/AiModel'
+import LoadingDialog from '../_components/LoadingDialog'
 
 function CourseLayout() {
   const params = useParams()
   const courseId = params.courseId
   const { user } = useUser()
   const [course, setCourse] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     courseId && GetCourse()
@@ -33,10 +37,40 @@ function CourseLayout() {
     console.log(result)
   }
 
+  const GenerateChapterContent = async () => {
+    setLoading(true)
+    const chapters = course?.courseOutput?.[0]?.chapters
+    chapters.forEach(async (chapter, index) => {
+      const PROMPT =
+        'Explain the concept in Detail on topic: ' +
+        course?.name +
+        ', chapter: ' +
+        chapter.chapter_name +
+        ', in JSON Format with list of array with field as title, explanation on given chapter in detail, Code Example(code field in <precode> format) if applicable'
+
+      if (index == 0) {
+        try {
+          const result = await GenerateCourseLayout_AI.sendMessage(PROMPT)
+
+          /* Response in text format */
+          console.log(result)
+
+          /* TO Convert into Json format --> JSON.parse(result) --> from text to json format */
+          console.log(JSON.parse(result))
+          setLoading(false)
+        } catch (error) {
+          setLoading(false)
+          console.log(error)
+        }
+      }
+    })
+  }
+
   return (
     <div className='mt-10 px-7 md:px-20 lg:px-44'>
       <h2 className='font-bold text-center text-2xl'>Course Layout</h2>
 
+      <LoadingDialog loading={loading} />
       {/* Basic Info */}
       <CourseBasicInfo course={course} refreshData={() => GetCourse()} />
 
@@ -45,6 +79,10 @@ function CourseLayout() {
 
       {/* List of Lesson */}
       <ChapterList course={course} refreshData={() => GetCourse()} />
+      <Button onClick={GenerateChapterContent} className='my-10'>
+        {' '}
+        Generate Course Content{' '}
+      </Button>
     </div>
   )
 }
