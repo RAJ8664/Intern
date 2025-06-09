495b398038f050869bf7ff25322f64a06089b6d1
RAJ ROY <rajr86642@gmail.com>
2025-06-10 02:42:51 +0545

    add delete course feature

--- course-generator/app/dashboard/_components/CourseCard.jsx
+++ course-generator/app/dashboard/_components/CourseCard.jsx
@@ -1,26 +1,52 @@
-import Image from 'next/image'
 import React from 'react'
-import { IoBookSharp } from "react-icons/io5";
+import { IoBookSharp } from 'react-icons/io5'
+import { HiOutlineBookOpen } from 'react-icons/hi2'
+import { HiMiniEllipsisVertical } from 'react-icons/hi2'
+import { db } from '@/configs/db'
+import { CourseList } from '@/configs/schema'
+import { eq } from 'drizzle-orm'
+import DropdownOption from './DropdownOption'
 
-function CourseCard({course}) {
+export function CourseCard({ course, refreshData }) {
+  const handleOnDelete = async () => {
+    const response = await db
+      .delete(CourseList)
+      .where(eq(CourseList.courseID, course?.courseID))
+      .returning({ courseID: CourseList?.courseID })
+    if (response) {
+      refreshData()
+    }
+  }
   return (
-    <div className='shadow-sm rounded-lg border p-2
-    hover:scale-105 transition-all cursor-pointer mt-4 '>
-        <Image src={course?.courseBanner} width={300} height={200}
+    <div
+      className='shadow-sm rounded-lg border p-2
+    hover:scale-105 transition-all cursor-pointer mt-4 '
+    >
+      <img
+        src={course?.courseBanner}
+        width={300}
+        height={200}
         className=' w-full h-[200px] object-cover rounded-lg'
-        />
-        <div className='p-2'>
-            <h2 className='font-medium text-lg'>{course?.courseOutput?.course?.name}</h2>
-            <p className='text-sm text-gray-400 my-1'>{course?.category}</p>
-            <div className='flex items-center justify-between'>   
-              <h2 className='flex gap-2 items-center p-1 bg-purple-50 text-primary text-sm rounded-sm '>
-               <HiOutlineBookOpen/> {course?.courseOutput?.course?.numberOfChapters} Chapters
-              </h2>
-              <h2 className='text-sm bg-purple-50 text-primary p-1 rounded-sm'>
-                {course?.level}     
-              </h2>
-            </div>
+      />
+      <div className='p-2'>
+        <h2 className='font-medium text-lg flex justify-between items-center'>
+          {course?.courseOutput[0]?.course_name}
+          <DropdownOption handleOnDelete={() => handleOnDelete()}>
+            {' '}
+            <HiMiniEllipsisVertical />
+          </DropdownOption>
+        </h2>
+        <p className='text-sm text-gray-400 my-1'>{course?.category}</p>
+        <div className='flex items-center justify-between'>
+          <h2 className='flex gap-2 items-center p-1 bg-purple-50 text-purple-400 text-sm rounded-sm '>
+            <HiOutlineBookOpen /> {course?.courseOutput[0]?.no_of_chapters}{' '}
+            Chapters
+          </h2>
+          <h2 className='text-sm bg-purple-50 text-purple-400 p-1 rounded-sm'>
+            {course?.level}
+          </h2>
         </div>
+      </div>
     </div>
   )
 }
--- course-generator/app/dashboard/_components/UserCourseList.jsx
+++ course-generator/app/dashboard/_components/UserCourseList.jsx
@@ -1,42 +1,41 @@
-"use client"
-
-
-import { db } from '@/configs/db';
-import { CourseList } from '@/configs/schema';
-import { useUser } from '@clerk/nextjs';
-import { eq } from 'drizzle-orm';
-import React, {useEffect, useState} from 'react'
-import CourseCard from './CourseCard';
+'use client'
+import { db } from '@/configs/db'
+import { CourseList } from '@/configs/schema'
+import { useUser } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm'
+import React, { useEffect, useState } from 'react'
+import CourseCard from './CourseCard'
 
 function UserCourseList() {
-  const [courseList,setCourseList]=useState([]);
-  const {user}= useUser();
- useEffect(()=>{
- user&&getUserCourses();
+  const [courseList, setCourseList] = useState([])
+  const { user } = useUser()
 
- },[user])
+  useEffect(() => {
+    user && getUserCourses()
+  }, [user])
 
-
-
-  const getUserCourses=async()=> {
-    const result = await db.select().from(CourseList)
-      .where(eq(CourseList?.createdBy,user?.primaryEmailAddress?.emailAddress));
-    console.log(result);
-    setCourseList(result);
-
+  const getUserCourses = async () => {
+    const result = await db
+      .select()
+      .from(CourseList)
+      .where(eq(CourseList?.createdBy, user?.primaryEmailAddress?.emailAddress))
+    setCourseList(result)
   }
+
   return (
     <div className='mt-10'>
       <h2 className='font-medium text-xl'>My AI Courses </h2>
-    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5'>{
-   courseList?.map((course,index)=>(
-    <CourseCard course={course} key={index}/>
-   ))}</div>
-    
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5'>
+        {courseList?.map((course, index) => (
+          <CourseCard
+            course={course}
+            key={index}
+            refreshData={() => getUserCourses()}
+          />
+        ))}
+      </div>
     </div>
   )
 }
 
 export default UserCourseList
-
-
