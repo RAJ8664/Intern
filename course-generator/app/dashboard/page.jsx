8abde573e689728c74c735b3cf9779f0c3b99f70
rkroy <rajr86642@gmail.com>
2025-10-02 16:43:23 +0530

    refactor

--- course-generator/app/dashboard/page.jsx
+++ course-generator/app/dashboard/page.jsx
@@ -1,16 +1,17 @@
+export const dynamic = 'force-dynamic'
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 import AddCourse from './_components/AddCourse'
 import UserCourseList from './_components/UserCourseList'
 
 function Dashboard() {
-  return (
-    <div>
-      <AddCourse />
-      {/*Display List of Course*/}
-      <UserCourseList />
-    </div>
-  )
+    return (
+        <div>
+            <AddCourse />
+            {/*Display List of Course*/}
+            <UserCourseList />
+        </div>
+    )
 }
 
 export default Dashboard
