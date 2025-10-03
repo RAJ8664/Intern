5d6602b9b770db654732d0fa5e9ff8e500699885
rkroy <rajr86642@gmail.com>
2025-10-03 11:09:44 +0530

    refactor

--- course-generator/next.config.mjs
+++ course-generator/next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
 	images: {
 		domains: ['img.clerk.com'],
 	},
+	experimental: {
+		serverComponentsExternalPackages: [
+			'@clerk/shared',
+		],
+	},
 }
 
 export default nextConfig
