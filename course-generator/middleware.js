61be7eeaaff19f38fed15ea5d640fb6843d6d27d
rkroy <rajr86642@gmail.com>
2025-10-03 13:17:55 +0530

    refactor

--- course-generator/middleware.js
+++ course-generator/middleware.js
@@ -1,3 +1,24 @@
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+//
+// const isProtectedRoute = createRouteMatcher([
+// 	"/dashboard(.*)",
+// 	"/create-course(.*)",
+// 	"/course(.*)",
+// ]);
+//
+// export default clerkMiddleware(async (auth, req) => {
+// 	if (isProtectedRoute(req)) await auth.protect();
+// });
+//
+// export const config = {
+// 	matcher: [
+// 		// Skip Next.js internals and all static files, unless found in search params
+// 		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+// 		// Always run for API routes
+// 		"/(api|trpc)(.*)",
+// 	],
+// };
+//
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -6,8 +27,10 @@ const isProtectedRoute = createRouteMatcher([
 	"/course(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
-	if (isProtectedRoute(req)) await auth.protect();
+export default clerkMiddleware((auth, req) => {
+	if (isProtectedRoute(req)) {
+		auth.protect();
+	}
 });
 
 export const config = {
