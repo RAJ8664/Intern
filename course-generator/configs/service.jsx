fe984bb79de93ca45564a621375264b1e5309b02
Rk Roy🇳🇵 <rajr86642@gmail.com>
2025-11-10 20:07:09 +0530

    Update : Fix youtube VideoId

--- course-generator/configs/service.jsx
+++ course-generator/configs/service.jsx
@@ -1,17 +1,43 @@
 export const dynamic = 'force-dynamic'
 import axios from 'axios'
 
+// const YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3'
+// const getVideos = async (query) => {
+//     const params = {
+//         part: 'snippet',
+//         q: query,
+//         maxResults: 1,
+//         type: 'video',
+//         videoDuration: 'long',
+//         key: process.env?.NEXT_PUBLIC_YOUTUBE_API_KEY,
+//     }
+//     const response = await axios.get(YOUTUBE_BASE_URL + '/search', { params })
+//     return response.data.items
+// }
+// export default { getVideos }
+//
+
 const YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3'
+
 const getVideos = async (query) => {
+    if (typeof window === 'undefined') return [] // Prevents running on server
+
+    const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY // safe to use in browser
     const params = {
         part: 'snippet',
         q: query,
         maxResults: 1,
         type: 'video',
         videoDuration: 'long',
-        key: process.env?.NEXT_PUBLIC_YOUTUBE_API_KEY,
+        key: API_KEY,
     }
-    const response = await axios.get(YOUTUBE_BASE_URL + '/search', { params })
-    return response.data.items
+
+    const response = await fetch(
+        `${YOUTUBE_BASE_URL}/search?${new URLSearchParams(params)}`,
+    )
+
+    const data = await response.json()
+    return data.items
 }
+
 export default { getVideos }
